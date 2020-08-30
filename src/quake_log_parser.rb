e23fe2b3ff46c878968bcc01a59daf79e52f09a9
Rene Soares <renecms@gmail.com>
2020-08-30 16:03:53 -0300

    Added regular expressions for more log messages

--- src/quake_log_parser.rb
+++ src/quake_log_parser.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
 class QuakeLogParser
-  REGEX_KILL = /(?<Hour>\d{1,2}):(?<Minutes>\d{1,2}) Kill: (?<KillerID>\d*) (?<DeadId>\d*) (?<CODId>\d*): (?<Killer>\S*) killed (?<Dead>\S*) by (?<COD>\S*)/.freeze
-  REGEX_ITEM = /(?<Hour>\d{1,2}):(?<Minutes>\d{1,2}) Item: (?<PlayerId>\d*) (?<Item>\S*)/.freeze
+  REGEX_KILL = /^(?<Hour>\d{1,2}):(?<Minutes>\d{1,2}) Kill: (?<KillerId>\d*) (?<DeadId>\d*) (?<CoD_Id>\d*): (?<Killer>\S*) killed (?<Dead>\S*) by (?<CoD>\S*)$/.freeze
+  REGEX_ITEM = /^(?<Hour>\d{1,2}):(?<Minutes>\d{1,2}) Item: (?<PlayerId>\d*) (?<Item>\S*)$/.freeze
+  REGEX_INIT_GAME = /^(?<Hour>\d{1,2}):(?<Minutes>\d{1,2}) InitGame: (?<GameConfig>\.*)$/.freeze
+  REGEX_CLIENT_CONNECTED = /^(?<Hour>\d{1,2}):(?<Minutes>\d{1,2}) ClientConnect: (?<PlayerId>\d*)$/.freeze
+  REGEX_CLIENT_INFO_CHANGED = /^(?<Hour>\d{1,2}):(?<Minutes>\d{1,2}) ClientUserinfoChanged: (?<PlayerId>\d*) (?<UserInfo>\S*)$/.freeze
+  REGEX_CLIENT_BEGIN = /^(?<Hour>\d{1,2}):(?<Minutes>\d{1,2}) ClientBegin: (?<PlayerId>\d*)$/.freeze
+  REGEX_SHUTDOWN_GAME = /^(?<Hour>\d{1,2}):(?<Minutes>\d{1,2}) ShutdownGame:$/.freeze
 
   def match_kill_line(line)
     line.match?(REGEX_KILL)
