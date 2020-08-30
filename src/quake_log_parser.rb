2eb35210a8e1a25aa9d1172f6dc185af5bf04a13
Rene Soares <renecms@gmail.com>
2020-08-30 15:49:47 -0300

    Created kill e item matchers and unit tests

--- src/quake_log_parser.rb
+++ src/quake_log_parser.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 class QuakeLogParser
-  def parse_line(_line)
-    1
+  REGEX_KILL = /(?<Hour>\d{1,2}):(?<Minutes>\d{1,2}) Kill: (?<KillerID>\d*) (?<DeadId>\d*) (?<CODId>\d*): (?<Killer>\S*) killed (?<Dead>\S*) by (?<COD>\S*)/.freeze
+  REGEX_ITEM = /(?<Hour>\d{1,2}):(?<Minutes>\d{1,2}) Item: (?<PlayerId>\d*) (?<Item>\S*)/.freeze
+
+  def match_kill_line(line)
+    line.match?(REGEX_KILL)
+  end
+
+  def match_item_line(line)
+    line.match?(REGEX_ITEM)
   end
 end
-
-puts QuakeLogParser.new.parse_line(1)
--- test/quake_log_parser_test.rb
+++ test/quake_log_parser_test.rb
@@ -5,7 +5,20 @@ require 'minitest/autorun'
 require 'test_helper'
 
 class QuakeLogParserTest < Minitest::Test
-  def test_single_kill_line
-    assert_equal 1, QuakeLogParser.new.parse_line('20:54 Kill: 1022 2 22: <world> killed Isgalamido by MOD_TRIGGER_HURT')
+  def test_kill_parser_success
+    assert_equal true, QuakeLogParser.new.match_kill_line('20:54 Kill: 1022 2 22: <world> killed Isgalamido by MOD_TRIGGER_HURT')
   end
+
+  def test_kill_parser_fail
+    assert_equal false, QuakeLogParser.new.match_kill_line('20:40 Item: 2 weapon_rocketlauncher')
+  end
+
+  def test_item_parser_success
+    assert_equal true, QuakeLogParser.new.match_item_line('20:40 Item: 2 weapon_rocketlauncher')
+  end
+
+  def test_item_parser_fail
+    assert_equal false, QuakeLogParser.new.match_item_line('20:54 Kill: 1022 2 22: <world> killed Isgalamido by MOD_TRIGGER_HURT')
+  end
+
 end
